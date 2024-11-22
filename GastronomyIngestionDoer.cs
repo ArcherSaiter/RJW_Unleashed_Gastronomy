36f39477e101be266d017bf63e8d7d86e26c15c0
ArcherSaiter <archersaiter.ss@gmail.com>
2024-11-22 00:22:19 +0000

    Add files via upload

---
+++ GastronomyIngestionDoer.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using Verse;
+using RJW_Unleashed_Framework;
+
+namespace RJW_Unleashed_Gastronomy
+{
+    public class RJW_Unleashed_GastronomyIngestionOutcomeDoer : IngestionOutcomeDoer
+    {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing thing, int ingestedcount)
+        {
+            if (thing == null) return;
+            if (pawn == null) return;
+            CompIngredients compIngredients = thing.TryGetComp<CompIngredients>();
+            if (compIngredients == null) return;
+            ThingDef thingDef = thing.def;
+
+            for (int i = 0; i < ingestedcount; i++)
+            {
+                foreach (ThingDef ingredient in compIngredients.ingredients)
+                {
+                    ApplyEffect.FullExtension(pawn, thingDef);
+                    if (thingDef.GetModExtension<TransformationAddon>() != null)
+                    {
+                        ApplyEffect.ExtensionApplierGenetic(pawn, thingDef);
+                    }
+                }
+            }
+        }
+    }
+}
